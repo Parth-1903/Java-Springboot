fa507fed2fb0548077d5708844a9a2e19f83e052
Parth-1903 <parththakkar1903@gmail.com>
2024-06-30 14:53:32 +0530

    First Commit

---
+++ Week_1_Spring_and_SpringBoot_Intro/Week_1_IMPL/week_1_spring_intro/src/main/java/com/spring_intro_01/week_1_spring_intro/Week1SpringIntroApplication.java
@@ -0,0 +1,13 @@
+package com.spring_intro_01.week_1_spring_intro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week1SpringIntroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week1SpringIntroApplication.class, args);
+	}
+
+}
---
+++ Week_1_Spring_and_SpringBoot_Intro/Week_1_IMPL/week_1_spring_intro/src/test/java/com/spring_intro_01/week_1_spring_intro/Week1SpringIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_intro_01.week_1_spring_intro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week1SpringIntroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
